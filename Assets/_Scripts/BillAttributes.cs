e7595d257e7c882a618c589e43f7872b7a3b9c8c
Jared Slawski <jaredslawski@gmail.com>
2025-10-06 16:14:50 -0600

    Bills and cards updated

--- Assets/_Scripts/BillAttributes.cs
+++ Assets/_Scripts/BillAttributes.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 public class BillAttributes : ScriptableObject
 {
     public string title;
-    public Sprite billImage;
     public int targetAmount;
     public int daysToComplete;
     public int punishmentAmount;
